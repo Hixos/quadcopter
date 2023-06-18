def2521ce8760c2a862977cda90c42236bae4db1
Luca Erbetta <luca.erbetta105@gmail.com>
2023-06-18 20:39:40 +0200

    Made "plottable" support multiple lines

--- examples/simple_pid.rs
+++ examples/simple_pid.rs
@@ -2,10 +2,12 @@ use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 use control_system::{
-    blocks::Delay, ControlBlock, ControlSystemBuilder, InputConnector, Interconnector,
-    OutputConnector, StepInfo,
+    blocks::{Delay, Generator},
+    numeric::{ForwardEuler, ODESolver, RungeKutta4},
+    ControlBlock, ControlSystemBuilder, InputConnector, Interconnector, OutputConnector, StepInfo,
 };
-use drone::plottable::{self, Plotter};
+use drone::plottable::Plotter;
+use nalgebra::SVector;
 use plotter::SignalGroup;
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -24,31 +26,54 @@ pub fn init_control(plot_signals: Arc<Mutex<SignalGroup>>) {
         // Generate reference input
         match k.t {
             t if t <= 1f64 => 0f64,
-            t if t > 1f64 && t <= 30f64 => 100f64,
-            t if t > 30f64 && t <= 60f64 => 50f64,
+            t if t > 1f64 && t <= 60f64 => 100f64,
+            t if t > 60f64 && t <= 120f64 => 50f64,
             _ => 0f64,
         }
     });
 
-    let pid_pos = PI::new("pid_pos", "p_ref", "p_val", "v_ref", 0.2f64, 0f64);
-    let pid_vel = PI::new("pid_vel", "v_ref", "v_val", "car/force", 5f64, 0.5f64);
-
+    let pid_pos = PI::new("pid_pos", "p_ref", "p_val", "v_ref", 0.3f64, 0.05f64);
+    let pid_vel = PI::new("pid_vel", "v_ref", "v_val", "car/force", 15f64, 0.5f64);
     let car = Car::new("car", 1f64, 10f64);
+    let delay_vel = Delay::<f64, 1>::new("vel_delay", 0f64, "car/vel", "v_val");
+    let delay_pos = Delay::<f64, 1>::new("pos_delay", 0f64, "car/pos", "p_val");
+
+    let eul_pid_pos = PI::new(
+        "eul/pid_pos",
+        "p_ref",
+        "eul/p_val",
+        "eul/v_ref",
+        0.3f64,
+        0.05f64,
+    );
+    let eul_pid_vel = PI::new(
+        "eul/pid_vel",
+        "eul/v_ref",
+        "eul/v_val",
+        "eul/car/force",
+        15f64,
+        0.5f64,
+    );
+    let eul_car = EulerCar::new("eul/car", 1f64, 10f64);
+    let eul_delay_vel = Delay::<f64, 1>::new("eul/vel_delay", 0f64, "eul/car/vel", "eul/v_val");
+    let eul_delay_pos = Delay::<f64, 1>::new("eul/pos_delay", 0f64, "eul/car/pos", "eul/p_val");
 
     let mut builder = ControlSystemBuilder::new();
 
     builder.add_block(gen).unwrap();
+
     builder.add_block(pid_vel).unwrap();
     builder.add_block(pid_pos).unwrap();
     builder.add_block(car).unwrap();
+    builder.add_block(delay_vel).unwrap();
+    builder.add_block(delay_pos).unwrap();
 
-    builder
-        .add_block(Delay::<f64, 1>::new("vel_delay", 0f64, "car/vel", "v_val"))
-        .unwrap();
+    builder.add_block(eul_pid_vel).unwrap();
+    builder.add_block(eul_pid_pos).unwrap();
+    builder.add_block(eul_car).unwrap();
+    builder.add_block(eul_delay_vel).unwrap();
+    builder.add_block(eul_delay_pos).unwrap();
 
-    builder
-        .add_block(Delay::<f64, 1>::new("pos_delay", 0f64, "car/pos", "p_val"))
-        .unwrap();
 
     let mut add_probe = |sig_name: &str| {
         builder
@@ -62,9 +87,19 @@ pub fn init_control(plot_signals: Arc<Mutex<SignalGroup>>) {
     add_probe("car/vel");
     add_probe("car/force");
 
+    add_probe("eul/v_ref");
+    add_probe("eul/car/pos");
+    add_probe("eul/car/vel");
+    add_probe("eul/car/force");
+
+
+
+    let dt = 10; // ms
+    let n = 200000/dt;
+
     let mut cs = builder.build(0f64).unwrap();
-    for _ in 0..1000 {
-        cs.step(0.1).unwrap();
+    for _ in 0..n {
+        cs.step(dt as f64 / 1000f64).unwrap();
     }
 }
 
@@ -80,8 +115,7 @@ pub struct Car {
     m: f64,
 
     // State
-    x: f64,
-    v: f64,
+    x: SVector<f64, 2>, // x, v
 }
 
 impl Car {
@@ -93,8 +127,7 @@ impl Car {
             y_v: OutputConnector::new(format!("{}/vel", block_name).as_str()),
             k,
             m,
-            x: 0f64,
-            v: 0f64,
+            x: SVector::<f64, 2>::new(0f64, 0f64),
         }
     }
 }
@@ -111,14 +144,83 @@ impl ControlBlock for Car {
 
     #[allow(unused_variables)]
     fn step(&mut self, k: StepInfo) -> Result<()> {
-        let F = self.u_f.input().unwrap();
-        let a = (F - self.k * self.v.abs()) / self.m;
+        let f = self.u_f.input().unwrap();
+        self.x = RungeKutta4::solve(
+            |t0, y0| {
+                let v = y0[1];
+                let a = (f - self.k * v.abs()) / self.m;
+                SVector::<f64, 2>::new(v, a)
+            },
+            k.t,
+            k.dt,
+            self.x,
+        );
 
-        self.v += a * k.dt;
-        self.x += self.v * k.dt;
+        self.y_x.output(self.x[0]);
+        self.y_v.output(self.x[1]);
+        Ok(())
+    }
 
-        self.y_x.output(self.x);
-        self.y_v.output(self.v);
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+pub struct EulerCar {
+    name: String,
+
+    u_f: InputConnector<f64>,
+    y_x: OutputConnector<f64>,
+    y_v: OutputConnector<f64>,
+
+    // Params
+    k: f64,
+    m: f64,
+
+    // State
+    x: SVector<f64, 2>, // x, v
+}
+
+impl EulerCar {
+    pub fn new(block_name: &str, k: f64, m: f64) -> Self {
+        EulerCar {
+            name: block_name.to_string(),
+            u_f: InputConnector::new(format!("{}/force", block_name).as_str()),
+            y_x: OutputConnector::new(format!("{}/pos", block_name).as_str()),
+            y_v: OutputConnector::new(format!("{}/vel", block_name).as_str()),
+            k,
+            m,
+            x: SVector::<f64, 2>::new(0f64, 0f64),
+        }
+    }
+}
+
+impl ControlBlock for EulerCar {
+    fn register_inputs(&mut self, interconnector: &mut Interconnector) -> Result<()> {
+        interconnector.register_input(&mut self.u_f)
+    }
+
+    fn register_outputs(&mut self, interconnector: &mut Interconnector) -> Result<()> {
+        interconnector.register_output(&mut self.y_x)?;
+        interconnector.register_output(&mut self.y_v)
+    }
+
+    #[allow(unused_variables)]
+    fn step(&mut self, k: StepInfo) -> Result<()> {
+        let f = self.u_f.input().unwrap();
+        self.x = ForwardEuler::solve(
+            |t0, y0| {
+                let v = y0[1];
+                let a = (f - self.k * v.abs()) / self.m;
+                SVector::<f64, 2>::new(v, a)
+            },
+            k.t,
+            k.dt,
+            self.x,
+        );
+
+        self.y_x.output(self.x[0]);
+        self.y_v.output(self.x[1]);
         Ok(())
     }
 
@@ -179,56 +281,6 @@ impl ControlBlock for PI {
         self.acc += err * k.dt;
 
         self.y.output(self.k * err + self.ki * self.acc);
-        Ok(())
-    }
-
-    fn name(&self) -> String {
-        self.name.clone()
-    }
-}
-
-struct Generator<T, F>
-where
-    T: Copy,
-    F: Fn(StepInfo) -> T,
-{
-    name: String,
-    y: OutputConnector<T>,
-
-    f: F,
-}
-
-impl<T, F> Generator<T, F>
-where
-    T: Copy,
-    F: Fn(StepInfo) -> T,
-{
-    fn new(name: &str, y_name: &str, f: F) -> Self {
-        Generator {
-            name: name.to_string(),
-            y: OutputConnector::new(y_name),
-            f: f,
-        }
-    }
-}
-
-impl<T, F> ControlBlock for Generator<T, F>
-where
-    T: Copy + 'static,
-    F: Fn(StepInfo) -> T,
-{
-    #[allow(unused_variables)]
-    fn register_inputs(&mut self, interconnector: &mut Interconnector) -> Result<()> {
-        Ok(())
-    }
-
-    fn register_outputs(&mut self, interconnector: &mut Interconnector) -> Result<()> {
-        interconnector.register_output(&mut self.y)
-    }
-
-    #[allow(unused_variables)]
-    fn step(&mut self, k: StepInfo) -> Result<()> {
-        self.y.output((self.f)(k));
         Ok(())
     }
 
--- src/plottable.rs
+++ src/plottable.rs
@@ -2,31 +2,42 @@ use control_system::{Prober, StepInfo};
 use plotter::{SignalGroup, SignalSample};
 use std::sync::{mpsc::Sender, Mutex};
 
-pub trait Plottable {
-    fn plot(&self, t: f64, plot_signal: &Sender<SignalSample>);
+pub trait Plottable<const N: usize> {
+    fn plot(&self, t: f64, plot_signals: &[Sender<SignalSample>; N]);
 }
 
-pub struct Plotter {
-    signal: Sender<SignalSample>,
+pub struct Plotter<const N: usize> {
+    signals: [Sender<SignalSample>; N],
 }
 
-impl Plotter {
+impl<const N: usize> Plotter<N> {
     pub fn new(name: &str, signal_group: &Mutex<SignalGroup>) -> Self {
-        Plotter {
-            signal: signal_group.lock().unwrap().add_signal(name),
+        if N == 1 {
+            Plotter {
+                signals: std::array::from_fn(|_| signal_group.lock().unwrap().add_signal(name)),
+            }
+        } else {
+            Plotter {
+                signals: std::array::from_fn(|i| {
+                    signal_group
+                        .lock()
+                        .unwrap()
+                        .add_signal(format!("{name}/{name}[{}]", i, name=name).as_str())
+                }),
+            }
         }
     }
 }
 
-impl<T: Plottable> Prober<T> for Plotter {
+impl<const N: usize, T: Plottable<N>> Prober<T> for Plotter<N> {
     fn probe(&self, _: &str, v: Option<T>, k: StepInfo) {
-        v.unwrap().plot(k.t, &self.signal);
+        v.unwrap().plot(k.t, &self.signals);
     }
 }
 
-impl<T: Into<f64> + Clone> Plottable for T {
-    fn plot(&self, t: f64, plot_signal: &Sender<SignalSample>) {
-        plot_signal
+impl<T: Into<f64> + Clone> Plottable<1> for T {
+    fn plot(&self, t: f64, plot_signals: &[Sender<SignalSample>; 1]) {
+        plot_signals[0]
             .send(SignalSample {
                 t: t,
                 y: Into::<f64>::into(self.clone()),
